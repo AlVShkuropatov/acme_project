540f7d8ad1c55d8db81e44c28003adb0b2a2c6de
Александр Ш <AlexMenshikov500@yandex.ru>
2023-12-31 17:39:05 +0400

    mixin ballin

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import get_object_or_404, redirect, render
 from .forms import BirthdayForm
 from .utils import calculate_birthday_countdown
 from .models import Birthday
+from django.core.paginator import Paginator
+from django.views.generic import ListView, DeleteView, CreateView, UpdateView
+from django.urls import reverse_lazy
+
 
 def birthday(request, pk=None):
     if pk is not None:
@@ -27,9 +31,12 @@ def birthday(request, pk=None):
 
 def birthday_list(request):
     # Получаем все объекты модели Birthday из БД.
-    birthdays = Birthday.objects.all()
+    birthdays = Birthday.objects.order_by('id')
+    paginator = Paginator(birthdays, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     # Передаём их в контекст шаблона.
-    context = {'birthdays': birthdays}
+    context = {'page_obj': page_obj}
     return render(request, 'birthday/birthday_list.html', context) 
 
 def delete_birthday(request, pk):
@@ -47,3 +54,27 @@ def delete_birthday(request, pk):
         return redirect('birthday:list')
     # Если был получен GET-запрос — отображаем форму.
     return render(request, 'birthday/birthday.html', context)
+
+# Создаём миксин.
+class BirthdayMixin:
+    model = Birthday
+    form_class = BirthdayForm
+    template_name = 'birthday/birthday.html'
+    success_url = reverse_lazy('birthday:list')
+
+
+class BirthdayListView(ListView):
+    model = Birthday
+    ordering = 'id'
+    paginate_by = 10
+
+
+class BirthdayCreateView(BirthdayMixin, CreateView):
+    pass
+
+class BirthdayUpdateView(BirthdayMixin, UpdateView):
+    pass
+
+class BirthdayDeleteView(DeleteView):
+    model = Birthday
+    success_url = reverse_lazy('birthday:list')
